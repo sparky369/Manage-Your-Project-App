2a301da786962157b8ef3d1b64b2f91ae96a574a
Shahriar <lonelyuchass@gmail.com>
2017-09-09 19:00:41 +0600

    projects for tenants

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -4,6 +4,7 @@ class Tenant < ApplicationRecord
   has_many :members, dependent: :destroy
   validates_presence_of :name
   validates_uniqueness_of :name
+  has_many :projects, dependent: :destroy
 
 
     def self.create_new_tenant(tenant_params, user_params, coupon_params)
